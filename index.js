7d0f8ddc325c2336a2c5a225207ccb014c83e896
arbuzovwoi <idinaxer11115@gmail.com>
2026-05-26 10:22:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ io.on('connection', (socket) => {
     socket.lobbyId = lobbyId;
     socket.emit('lobbyCreated', { lobby });
     io.emit('lobbyListUpdate', getLobbyList());
+    
+    // Если лобби рассчитано на 1 игрока (бот?), но обычно нет, но на всякий случай
+    if (lobby.players.length >= lobby.maxPlayers) {
+      lobby.state = 'playing';
+      io.to(lobby.id).emit('gameStart', { lobby });
+      io.emit('lobbyListUpdate', getLobbyList());
+    }
   });
 
   socket.on('joinLobby', ({ lobbyId, playerName }) => {
@@ -56,6 +63,13 @@ io.on('connection', (socket) => {
     socket.emit('joinedLobby', lobby);
     io.to(lobbyId).emit('lobbyUpdated', lobby);
     io.emit('lobbyListUpdate', getLobbyList());
+    
+    // АВТОСТАРТ: если набралось максимальное количество игроков, начинаем игру
+    if (lobby.players.length >= lobby.maxPlayers) {
+      lobby.state = 'playing';
+      io.to(lobby.id).emit('gameStart', { lobby });
+      io.emit('lobbyListUpdate', getLobbyList());
+    }
   });
 
   socket.on('startGame', () => {
@@ -80,8 +94,12 @@ io.on('connection', (socket) => {
     if (lobby) {
       lobby.players = lobby.players.filter(p => p.id !== socket.id);
       socket.leave(lobbyId);
-      if (lobby.players.length === 0) lobbies.delete(lobbyId);
-      else io.to(lobbyId).emit('lobbyUpdated', lobby);
+      if (lobby.players.length === 0) {
+        lobbies.delete(lobbyId);
+      } else {
+        io.to(lobbyId).emit('lobbyUpdated', lobby);
+        io.to(lobbyId).emit('playerLeft', socket.id);
+      }
       io.emit('lobbyListUpdate', getLobbyList());
     }
     socket.lobbyId = null;
@@ -91,8 +109,12 @@ io.on('connection', (socket) => {
     const lobby = lobbies.get(socket.lobbyId);
     if (lobby) {
       lobby.players = lobby.players.filter(p => p.id !== socket.id);
-      if (lobby.players.length === 0) lobbies.delete(socket.lobbyId);
-      else io.to(socket.lobbyId).emit('lobbyUpdated', lobby);
+      if (lobby.players.length === 0) {
+        lobbies.delete(socket.lobbyId);
+      } else {
+        io.to(socket.lobbyId).emit('lobbyUpdated', lobby);
+        io.to(socket.lobbyId).emit('playerLeft', socket.id);
+      }
       io.emit('lobbyListUpdate', getLobbyList());
     }
   });
