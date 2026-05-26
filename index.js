28f1cffc1d4f5a45313550068936e6d547db6021
arbuzovwoi <idinaxer11115@gmail.com>
2026-05-26 10:32:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,13 +42,6 @@ io.on('connection', (socket) => {
     socket.lobbyId = lobbyId;
     socket.emit('lobbyCreated', { lobby });
     io.emit('lobbyListUpdate', getLobbyList());
-    
-    // Если лобби рассчитано на 1 игрока (бот?), но обычно нет, но на всякий случай
-    if (lobby.players.length >= lobby.maxPlayers) {
-      lobby.state = 'playing';
-      io.to(lobby.id).emit('gameStart', { lobby });
-      io.emit('lobbyListUpdate', getLobbyList());
-    }
   });
 
   socket.on('joinLobby', ({ lobbyId, playerName }) => {
@@ -63,13 +56,6 @@ io.on('connection', (socket) => {
     socket.emit('joinedLobby', lobby);
     io.to(lobbyId).emit('lobbyUpdated', lobby);
     io.emit('lobbyListUpdate', getLobbyList());
-    
-    // АВТОСТАРТ: если набралось максимальное количество игроков, начинаем игру
-    if (lobby.players.length >= lobby.maxPlayers) {
-      lobby.state = 'playing';
-      io.to(lobby.id).emit('gameStart', { lobby });
-      io.emit('lobbyListUpdate', getLobbyList());
-    }
   });
 
   socket.on('startGame', () => {
@@ -81,6 +67,7 @@ io.on('connection', (socket) => {
     }
   });
 
+  // Синхронизация движения и состояния
   socket.on('syncClient', (data) => {
     socket.to(socket.lobbyId).emit('syncServer', { id: socket.id, ...data });
   });
@@ -89,22 +76,56 @@ io.on('connection', (socket) => {
     socket.to(socket.lobbyId).emit('enemyShoot', { id: socket.id, ...data });
   });
 
+  // Урон от пули
+  socket.on('bulletHit', ({ targetId, damage }) => {
+    socket.to(socket.lobbyId).emit('damageInflicted', { targetId, damage });
+  });
+
+  // Удар кулаком
+  socket.on('meleeHit', ({ targetId, damage, isVamp }) => {
+    socket.to(socket.lobbyId).emit('meleeHitSync', { targetId, damage });
+  });
+
+  // Окончание раунда
+  socket.on('roundEnd', ({ winner, score }) => {
+    socket.to(socket.lobbyId).emit('roundEndSync', { winner, score });
+  });
+
+  // Синхронизация способностей
+  socket.on('abilityUsed', (data) => {
+    socket.to(socket.lobbyId).emit('abilitySync', { playerId: socket.id, ...data });
+  });
+
+  // Синхронизация номера раунда
+  socket.on('syncRound', ({ roundNumber }) => {
+    socket.to(socket.lobbyId).emit('roundNumberSync', roundNumber);
+  });
+
   socket.on('leaveLobby', ({ lobbyId }) => {
     const lobby = lobbies.get(lobbyId);
     if (lobby) {
       lobby.players = lobby.players.filter(p => p.id !== socket.id);
       socket.leave(lobbyId);
-      if (lobby.players.length === 0) {
-        lobbies.delete(lobbyId);
-      } else {
-        io.to(lobbyId).emit('lobbyUpdated', lobby);
-        io.to(lobbyId).emit('playerLeft', socket.id);
-      }
+      if (lobby.players.length === 0) lobbies.delete(lobbyId);
+      else io.to(lobbyId).emit('lobbyUpdated', lobby);
       io.emit('lobbyListUpdate', getLobbyList());
     }
     socket.lobbyId = null;
   });
 
+  socket.on('disconnect', () => {
+    const lobby = lobbies.get(socket.lobbyId);
+    if (lobby) {
+      lobby.players = lobby.players.filter(p => p.id !== socket.id);
+      if (lobby.players.length === 0) lobbies.delete(socket.lobbyId);
+      else io.to(socket.lobbyId).emit('lobbyUpdated', lobby);
+      io.emit('lobbyListUpdate', getLobbyList());
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   socket.on('disconnect', () => {
     const lobby = lobbies.get(socket.lobbyId);
     if (lobby) {
