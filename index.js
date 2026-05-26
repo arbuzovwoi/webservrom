d2eefc6da679dfe37b8e1cc80568bd73bd042db6
arbuzovwoi <idinaxer11115@gmail.com>
2026-05-26 08:40:17 +0300

    Updatew index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const lobbies = new Map();
 
 app.get('/', (req, res) => res.send('DAUN Fighter Server Online'));
 
+// Функция для списка лобби (только ожидающие)
 function getLobbyList() {
   return Array.from(lobbies.values())
     .filter(l => l.state === 'waiting')
@@ -20,48 +21,49 @@ function getLobbyList() {
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
+  // 👇 ОТПРАВЛЯЕМ СПИСОК СРАЗУ ПРИ ПОДКЛЮЧЕНИИ
+  socket.emit('lobbyList', getLobbyList());
+
+  // 👇 ОБРАБОТКА ЗАПРОСА ОТ КЛИЕНТА (если нужно обновить вручную)
+  socket.on('getLobbies', () => {
+    socket.emit('lobbyList', getLobbyList());
+  });
+
+  // Создание лобби
   socket.on('createLobby', (data) => {
     const { lobbyName, maxPlayers, map, playerName, mode } = data;
     const lobbyId = socket.id + Date.now().toString(36);
     const lobby = {
       id: lobbyId,
       name: lobbyName,
-      maxPlayers: mode === 'br' ? 6 : (maxPlayers || 2),
+      maxPlayers: maxPlayers || 2,
       map,
-      mode: mode || 'normal',
+      mode: mode || '1v1',
       players: [{ id: socket.id, name: playerName, hp: 100, isHost: true }],
       state: 'waiting',
-      seed: Math.random()
     };
     lobbies.set(lobbyId, lobby);
     socket.join(lobbyId);
     socket.lobbyId = lobbyId;
     
-    // Отправляем создателю событие lobbyCreated
     socket.emit('lobbyCreated', { lobby });
-    // Обновляем список лобби для всех
     io.emit('lobbyListUpdate', getLobbyList());
   });
 
+  // Присоединение к лобби
   socket.on('joinLobby', ({ lobbyId, playerName }) => {
     const lobby = lobbies.get(lobbyId);
-    if (!lobby || lobby.players.length >= lobby.maxPlayers || lobby.state !== 'waiting') {
-      socket.emit('joinError', { message: 'Cannot join lobby' });
-      return;
-    }
+    if (!lobby || lobby.players.length >= lobby.maxPlayers || lobby.state !== 'waiting') return;
 
     lobby.players.push({ id: socket.id, name: playerName, hp: 100, isHost: false });
     socket.join(lobbyId);
     socket.lobbyId = lobbyId;
 
-    // Отправляем присоединившемуся событие joinedLobby
-    socket.emit('joinedLobby', lobby);
-    // Всем остальным в комнате отправляем lobbyUpdated
-    socket.to(lobbyId).emit('lobbyUpdated', lobby);
-    // Обновляем список лобби
+    io.to(lobbyId).emit('lobbyUpdated', lobby);
     io.emit('lobbyListUpdate', getLobbyList());
   });
 
+  // Запуск игры (хост)
   socket.on('startGame', () => {
     const lobby = lobbies.get(socket.lobbyId);
     if (lobby && lobby.players[0].id === socket.id) {
@@ -71,13 +73,13 @@ io.on('connection', (socket) => {
     }
   });
 
-  // Синхронизация движения, стрельбы и т.д.
+  // Синхронизация движения и стрельбы (как у вас было)
   socket.on('syncClient', (data) => {
     socket.to(socket.lobbyId).emit('syncServer', { id: socket.id, ...data });
   });
 
   socket.on('shoot', (data) => {
-    socket.to(socket.lobbyId).emit('enemyShoot', data);
+    socket.to(socket.lobbyId).emit('enemyShoot', { id: socket.id, ...data });
   });
 
   socket.on('disconnect', () => {
@@ -87,12 +89,8 @@ io.on('connection', (socket) => {
       if (lobby.players.length === 0) {
         lobbies.delete(socket.lobbyId);
       } else {
-        // Уведомляем остальных об изменении состава
         io.to(socket.lobbyId).emit('lobbyUpdated', lobby);
-        // Если игра уже началась, отправляем сигнал о выходе
-        if (lobby.state === 'playing') {
-          io.to(socket.lobbyId).emit('playerLeft', { id: socket.id });
-        }
+        io.to(socket.lobbyId).emit('playerLeft', socket.id);
       }
       io.emit('lobbyListUpdate', getLobbyList());
     }
